ba7d0844256cebd56d2b1728ed74c364515f6851
mtalhaamjad <103028212+m-talha-amjad@users.noreply.github.com>
2023-02-06 04:16:21 +0500

    Update collapsible-custom.js

--- collapsible-custom.js
+++ collapsible-custom.js
@@ -1,42 +1,40 @@
 {
+  
+const mobileNavLink = document.querySelectorAll('.mobile-nav__link');
+
 const toggleButtons = document.querySelectorAll('.collapsible-toggle');
 
 function toggleCollapsible(event) {
     event.stopPropagation();
 
-  let sibling = this.nextElementSibling;
+  const content = this.nextElementSibling;
   const isExpanded = this.classList.contains("cus-active");
-  let foundSibling;
-
-  while (sibling) {
-    if (sibling.classList.contains('collapsible')) {
-      foundSibling = sibling;
-      break;
-    }
-    sibling = sibling.nextElementSibling;
-  }
-
-  if (foundSibling) {
-    console.log(foundSibling);
-    // use foundSibling here
-  }
 
   toggleButtons.forEach((button) => {
     if (button !== this) {
       button.classList.remove("cus-active");
       button.setAttribute('aria-expanded', false);
-      foundSibling.classList.remove("cus-active");
-      foundSibling.style.height = 0;
-      foundSibling.style.overflow = "hidden";
+      button.nextElementSibling.classList.remove("cus-active");
+      button.nextElementSibling.style.height = 0;
+      setTimeout(function () {
+        button.nextElementSibling.style.overflow = "hidden";
+      }, 500);
     }
   });
 
   this.setAttribute('aria-expanded', true);
-  !isExpanded ? foundSibling.classList.add("cus-active") : foundSibling.classList.remove("cus-active");
+  !isExpanded ? content.classList.add("cus-active") : content.classList.remove("cus-active");
   !isExpanded ? this.classList.add("cus-active") : this.classList.remove("cus-active");
-  foundSibling.style.height = !isExpanded ? foundSibling.scrollHeight + 'px' : 0;
-  foundSibling.style.overflow = !isExpanded ? "visible" : "hidden";
+  content.style.height = !isExpanded ? content.scrollHeight + 'px' : 0;
+  setTimeout(function () {
+    content.style.overflow = !isExpanded ? "visible" : "hidden";
+
+   }, 500);
 }
+
+mobileNavLink.forEach((button) => {
+  button.addEventListener('click', toggleCollapsible);
+});
 
 toggleButtons.forEach((button) => {
   button.addEventListener('click', toggleCollapsible);
