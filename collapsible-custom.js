c095579c9e9434753f32e7d59ab8fb0d47ff0b29
mtalhaamjad <103028212+m-talha-amjad@users.noreply.github.com>
2023-02-09 01:09:45 +0500

    Update collapsible-custom.js

--- collapsible-custom.js
+++ collapsible-custom.js
@@ -1,42 +1,109 @@
+// Focal 9.0 
+// both simple and rerender dom
+// =============Simple=========
 {
   
-const mobileNavLink = document.querySelectorAll('.mobile-nav__link');
+  const mobileNavLink = document.querySelectorAll('.mobile-nav__link');
 
-const toggleButtons = document.querySelectorAll('.collapsible-toggle');
+  const toggleButtons = document.querySelectorAll('body .collapsible-toggle');
 
-function toggleCollapsible(event) {
+  function toggleCollapsible(event) {
     event.stopPropagation();
 
-  const content = this.nextElementSibling;
-  const isExpanded = this.classList.contains("cus-active");
+    const content = this.nextElementSibling;
+    const isExpanded = this.classList.contains("cus-active");
 
-  toggleButtons.forEach((button) => {
-    if (button !== this) {
-      button.classList.remove("cus-active");
-      button.setAttribute('aria-expanded', false);
-      button.nextElementSibling.classList.remove("cus-active");
-      button.nextElementSibling.style.height = 0;
-      setTimeout(function () {
-        button.nextElementSibling.style.overflow = "hidden";
-      }, 500);
-    }
+    toggleButtons.forEach((button) => {
+      if (button !== this) {
+        button.classList.remove("cus-active");
+        button.setAttribute('aria-expanded', false);
+        button.nextElementSibling.classList.remove("cus-active");
+        button.nextElementSibling.style.height = 0;
+        setTimeout(function () {
+          button.nextElementSibling.style.overflow = "hidden";
+        }, 500);
+      }
+    });
+
+    
+    !isExpanded ? content.classList.add("cus-active") : content.classList.remove("cus-active");
+    !isExpanded ? this.classList.add("cus-active") : this.classList.remove("cus-active");
+    !isExpanded ? this.setAttribute('aria-expanded', true) : this.setAttribute('aria-expanded', false);
+    content.style.height = !isExpanded ? content.scrollHeight + 'px' : 0;
+    setTimeout(function () {
+      content.style.overflow = !isExpanded ? "visible" : "hidden";
+
+    }, 500);
+  }
+
+  
+
+  mobileNavLink.forEach((button) => {
+    button.addEventListener('click', toggleCollapsible);
   });
 
-  this.setAttribute('aria-expanded', true);
-  !isExpanded ? content.classList.add("cus-active") : content.classList.remove("cus-active");
-  !isExpanded ? this.classList.add("cus-active") : this.classList.remove("cus-active");
-  content.style.height = !isExpanded ? content.scrollHeight + 'px' : 0;
-  setTimeout(function () {
-    content.style.overflow = !isExpanded ? "visible" : "hidden";
-
-   }, 500);
+  toggleButtons.forEach((button) => {
+    button.addEventListener('click', toggleCollapsible);
+  });
 }
+// =============Mutation Observer=========
+{
+  let targetElement = document.getElementById("facet-filters");
+  if (targetElement) {
+    window.addEventListener('DOMContentLoaded', (event) => {
+      console.log('DOM fully loaded and parsed');
 
-mobileNavLink.forEach((button) => {
-  button.addEventListener('click', toggleCollapsible);
-});
+      const observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+          if (mutation.addedNodes.length) {
+            mutation.addedNodes.forEach(node => {
+              console.log("dom changed");
+              const mobileNavLink = document.querySelectorAll('.mobile-nav__link');
 
-toggleButtons.forEach((button) => {
-  button.addEventListener('click', toggleCollapsible);
-});
+              const toggleButtons = document.querySelectorAll('body .collapsible-toggle');
+        
+              function toggleCollapsible(event) {
+                event.stopPropagation();
+        
+                const content = this.nextElementSibling;
+                const isExpanded = this.classList.contains("cus-active");
+        
+                toggleButtons.forEach((button) => {
+                  if (button !== this) {
+                    button.classList.remove("cus-active");
+                    button.setAttribute('aria-expanded', false);
+                    button.nextElementSibling.classList.remove("cus-active");
+                    button.nextElementSibling.style.height = 0;
+                    setTimeout(function () {
+                      button.nextElementSibling.style.overflow = "hidden";
+                    }, 500);
+                  }
+                });
+        
+                !isExpanded ? content.classList.add("cus-active") : content.classList.remove("cus-active");
+                !isExpanded ? this.classList.add("cus-active") : this.classList.remove("cus-active");
+                !isExpanded ? this.setAttribute('aria-expanded', true) : this.setAttribute('aria-expanded', false);
+                content.style.height = !isExpanded ? content.scrollHeight + 'px' : 0;
+                setTimeout(function () {
+                  content.style.overflow = !isExpanded ? "visible" : "hidden";
+        
+                }, 500);
+              }
+        
+              mobileNavLink.forEach((button) => {
+                button.addEventListener('click', toggleCollapsible);
+              });
+        
+              toggleButtons.forEach((button) => {
+                button.addEventListener('click', toggleCollapsible);
+              });
+            });
+          }
+        });
+      });
+
+      observer.observe(targetElement, { childList: true, subtree: true });
+    });
+  
+  }
 }
